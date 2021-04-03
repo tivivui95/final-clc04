f14b1b170eb8c241ee6ebd28d241e9825d53f110
NguyenMinhTuan <79949229+20127092-KTLT-NguyenMinhTuan@users.noreply.github.com>
2021-04-03 09:56:12 +0700

    Add files via upload

---
+++ student dashboard.cpp
@@ -0,0 +1,19 @@
+#include "Header.h"
+
+void studentDashboard()
+{
+	int choice;
+	cout << "1.Enroll" << endl;
+	cout << "2.Manage Class " << endl;
+	cout << "3.Manage course" << endl;
+	cout << "4.Manage score" << endl;
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	}
+		
+}
