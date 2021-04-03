9c9d886fa1f8f0408a8828de77f34d3450906235
Minh Van Nguyen <van23112002@gmail.com>
2021-04-03 23:39:20 +0700

    Change gitignore

--- draft/test.cpp
+++ draft/test.cpp
@@ -1,1 +1,12 @@
 // Có gì cần test mà ko muốn bỏ vô thì cứ code ở đây nha
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+    ifstream f;
+    string a;
+    f.open("text.txt");
+    f >> a; cout << a;
+    return 0;
+}
