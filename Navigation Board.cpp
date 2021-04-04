94ea47812973c6e51159196a4e5d4f6816c7bb53
Nguyen Manh Hung <nguyenmhung2310@gmail.com>
2021-04-04 10:21:14 +0700

    update
    update a little bit

--- Navigation Board.cpp
+++ Navigation Board.cpp
@@ -1,7 +1,6 @@
 #include"Header.h"
 
-
-void Navigation(int& choice)
+void Navigation(int& choice, ifstream& f)
 {
 	cout << "MOODLE\n";
 	cout << "1. Staff members\n";
@@ -17,38 +16,47 @@ void Navigation(int& choice)
 
 	switch (choice)
 	{
-	case 1:
-	{
-		cout << "STAFF LOGIN\n";
-		string check;
-	}
-	break;
+		case 1:
+		{
+			cout << "STAFF LOGIN\n";
+			string check;
+		}
+		break;
 
-	case 2:
-	{
-		cout << "STUDENT LOGIN\n";
-		string check;
-		cout << "Enter student id: ";
-		cin.ignore();
-		getline(cin, check);
-	}
-	break;
+		case 2:
+		{
+			cout << "STUDENT LOGIN\n";
+			string check;
+			cout << "Enter student id: ";
+			cin.ignore();
+			getline(cin, check);
+			Login(f, check);
+		}
+		break;
 
-	case 0:
-	{
-		cout << "Bye for hundreds times\n";
-		return;
+		case 0:
+		{
+			cout << "Bye for hundreds times\n";
+			return;
+		}
+		break;
 	}
-	break;
-	}
-
 }
 
 void Login(ifstream& f, string& k)
 {
 	f.open("loginData.txt", ios::in);
 	if (f.is_open())
+	{
 		cout << "Ready to go.\n";
+		string t;
+		while (f >> t)
+		{
+			if (k == t) {
+				cout << "logined successfully\n";
+			}
+		}
+	}
 	else
 	{
 		cerr << "Error\n";
