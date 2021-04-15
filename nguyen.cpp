b0a6603f5d506574d721607719a8290ed297a5f2
Nguyen Nguyen <khoinguyencubin@gmail.com>
2021-04-15 14:26:14 +0700

    Update nguyen.cpp

--- nguyen.cpp
+++ nguyen.cpp
@@ -1,12 +1,12 @@
 #include "Header.h"
 
-void printDetailsStudent(int i)
+void DStoClass(int i)
 {
     i = 1;
     cout << "Student name: " << Student.FirstName << endl;
     if (i)
     {
-        cout << "Class: " << Class.Name
+        cout << "Class: " << Class.Name << endl;
     }
 
 }
