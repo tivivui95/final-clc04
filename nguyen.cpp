3fa6f8bcfabfd1b99271d38876b0b37ac8f2fd29
Nguyen Nguyen <khoinguyencubin@gmail.com>
2021-04-21 10:47:25 +0700

    Update nguyen.cpp

--- nguyen.cpp
+++ nguyen.cpp
@@ -1,9 +1,21 @@
 #include "Header.h"
 
-template <typename N>
-class Classroom
+Person::Person(string name, string DoB, int No, int studentID, int socialID, bool gender)
 {
-public:
-    Classroom() { ; };
-    Classroom(string name, int no, int studentID, string gender, string DoB, int socialID);
-};
+    name_ = name;
+    No_ = No;
+    gender_ = gender;
+    DoB_ = DoB;
+    studentID_ = studentID;
+    socialID_ - socialID;
+}
+
+void Person::operator = (Person& p)
+{
+    this->name_ = p.name_;
+    this->No_ = p.no_;
+    this->gender_ = p.gender_;
+    this->DoB_ = p.DoB_;
+    this->studentID_ = p.studentID_;
+    this->socialID_ = p.socialID_;
+}
