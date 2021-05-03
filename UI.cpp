7692f126bcdd16b841beaa71b518d1d9c8d13020
Nguyen Manh Hung <nguyenmhung2310@gmail.com>
2021-05-03 20:53:33 +0700

    Update UI.cpp

--- UI.cpp
+++ UI.cpp
@@ -59,11 +59,7 @@ void UIgrid_1_1_Long(string string) {
 void UIgrid_1_1_Short(string string) {
     cout << "|" << setw(30) << string << setw(29) << "|" << flush << endl;
 }
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
+
 
 void SetConsoleTitle()
 {
@@ -97,7 +93,3 @@ void SetWindowSize(SHORT width, SHORT height)
 
     SetConsoleWindowInfo(hStdout, 1, &WindowSize);
 }
-<<<<<<< Updated upstream
-=======
->>>>>>> 7b179acb293dff4f00a0bb29c6de10a80c0133fb
->>>>>>> Stashed changes
