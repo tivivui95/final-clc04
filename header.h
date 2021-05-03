c0350fb16a5894dc0406df9445602ea92043227c
Minh Van Nguyen <van23112002@gmail.com>
2021-05-03 23:02:14 +0700

    Final

--- header.h
+++ header.h
@@ -13,12 +13,12 @@ using namespace std;
 
 #define MaxStudent 50;
 // Setting accent, Don't change anything in this class, it's for Vietnamese character
-typedef struct _SMALL_RECT {
-    SHORT Left;
-    SHORT Top;
-    SHORT Right;
-    SHORT Bottom;
-} SMALL_RECT;
+// typedef struct _SMALL_RECT {
+//     SHORT Left;
+//     SHORT Top;
+//     SHORT Right;
+//     SHORT Bottom;
+// } SMALL_RECT;
 
 using namespace std;
 
